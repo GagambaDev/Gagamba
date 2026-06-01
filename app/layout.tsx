11822181d754cf74a538968e1de32f14e98c9ed3
Narek Tonoyan <supersiblings03@gmail.com>
2026-05-31 22:38:32 -0700

    cleaned up font imports, dead vars, and rescoped ClerkProvider to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,8 @@
 import type { Metadata } from 'next'
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs'
-import { Geist, Geist_Mono, Syne, DM_Sans, Space_Grotesk } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+import { Syne, DM_Sans, Space_Grotesk } from 'next/font/google'
 import './globals.css'
 import Navbar from "@/components/Navbar";
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
 
 const syne = Syne({
   weight: ['400','600','700'],
@@ -47,11 +30,13 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className={`${syne.variable} ${dmSans.variable} ${spaceGrotesk.variable}`}>
-      <body>
-        <Navbar/>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className={`${syne.variable} ${dmSans.variable} ${spaceGrotesk.variable}`}>
+        <body>
+          <Navbar/>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
