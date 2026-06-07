59e272288c09aefc4f70e8e51c57e4b674c5b066
Dev Ada <250013605+ada-the-dev@users.noreply.github.com>
2026-06-07 16:03:27 -0700

    Added skeleton sections for About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 
-// Place any imports here
+// Import anything needed here
 
 export default function About() {
   return (
     <main className="min-h-screen bg-gradient-to-br from-ink-black to-dark-amethyst">
-      {/*issue C2: about page content goes here*/}
+      {/* Mission Statement */}
+      <section></section>
+
+      {/* Founders & Members */}
+      <section></section>
+
+      {/* Media from PIC and Company Events */}
+      <section></section>
+
     </main>
   );
 }
