6dd2d5e1e46efcb5d29e50c3edbe138014bf2893
Henok Wondem <wondemhenok@gmail.com>
2026-06-09 22:06:28 -0700

    Comparing and contrasting of traditional methods vs Gagamba's solutions

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import Hero from "@/components/Hero";
+import ProblemVsSolution from "@/components/ProblemVsSolution";
 import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
     <Hero/>
+    <ProblemVsSolution/>
+    </>
+    
   );
 }
