7d94bb1dda1a55e6a664d0a5bc6358423b535c17
BurningIcicles <richies2006huynh@gmail.com>
2026-06-09 21:00:36 -0700

    Added email sending

--- lib/email-templates.ts
+++ lib/email-templates.ts
@@ -3,3 +3,7 @@ import { Quote } from "@/types/forms/quote";
 export function quoteEmailBody(quote: Quote): string {
   return `Name: ${quote.name}\nEmail: ${quote.email}\n\n${quote.message}`;
 }
+
+export function quoteConfirmationEmailBody(quote: Quote): string {
+  return `Hi ${quote.name},\n\nThanks for reaching out to Team Gagamba! We've received your message and will get back to you soon.\n\nFor reference, here's what you sent us:\n\n${quote.message}\n\n-Team Gagamba`;
+}
