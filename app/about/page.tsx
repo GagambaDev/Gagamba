f6f3b867741820ae888918c3a956ef6b45be6728
Dev Ada <250013605+ada-the-dev@users.noreply.github.com>
2026-06-13 11:51:03 -0700

    Got rid of importing the PageTitle component because it is not being used. Placed all components outside of section tags

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import PageTitle from "@/components/PageTitle";
 import MissionStatement from "@/components/MissionStatement";
 import MediaGallery from "@/components/MediaGallery";
 import MeetTheTeam from "@/components/MeetTheTeam";
@@ -18,19 +17,15 @@ export default function About() {
         
         <div className="absolute inset-0 pointer-events-none" 
         style={{ background: "radial-gradient(ellipse 75% 65% at 20% 35%, rgba(22,65,185,0.9) 0%, rgba(12,35,110,0.4) 45%, transparent 70%)" }} />
-{/*         
-        <div className="mt-5">
-            <MissionStatement/>
-        </div> */}
         
         {/* Bottom portion of About page will fade into a darker background */}
         <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-[#050810] to-transparent pointer-events-none" />
         
       </section>
-      
-        <MissionStatement/>
-        <MeetTheTeam/>
-        <MediaGallery/>
+
+      <MissionStatement/>
+      <MeetTheTeam/>
+      <MediaGallery/>
       
     </div>
   );
